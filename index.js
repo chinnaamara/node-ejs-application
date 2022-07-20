13d9a4e500d09a2cf7677d797ebe212dc82e908a
chinnaamara <amara.chinna@gmail.com>
2022-07-20 12:31:19 +0530

    Added body parser

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const routes = require('./application');
 const app = express();
 const port = 3000;
 
-// const router = express.Router();
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
-// router.get('/', (req, res) => {
-//     res.send('Server running!');
-// });
+app.set("view engine", "ejs");
 
 app.use('/', routes.default);
 app.use('/book', routes.bookRoutes);
